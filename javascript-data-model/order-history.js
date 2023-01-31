c7aee0bbb9a59224fecd2cd7ba6816896de3880c
Bishan Webeshete <webesheteb@gmail.com>
2023-01-31 18:04:12 +0000

    completed javascript-data-model

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,65 @@
+var orderHistory = [
+  {
+    orderDate: 'August 4, 2020',
+    deliveryDate: 'August 8, 2020',
+    multipleItems: false,
+    totalPrice: 34,
+    recipient: 'JS Masher',
+    orderNumber: '114-3941689-8772232',
+    itemInfo: [{
+      item: 'JavaScript for impatient programmers',
+      itemSeller: 'Rauschmayer, Dr.Axel',
+      returnWindow: 'closed Sep 7, 2020',
+      listedPrice: 31.55
+    }]
+  },
+  {
+    orderDate: 'July 19, 2020',
+    deliveryDate: 'July 20, 2020',
+    multipleItems: false,
+    totalPrice: 44.53,
+    recipient: 'JS Masher',
+    orderNumber: '113-9984268-1280257',
+    itemInfo: [{
+      item: 'The Timeless Way pof Building',
+      itemSeller: 'Alexander, Christopher',
+      returnWindow: 'closed Aug 19, 2020',
+      listedPrice: 41.33
+    }]
+  },
+  {
+    orderDate: 'July 4, 2020',
+    deliveryDate: 'Jul 7, 2020',
+    multipleItems: false,
+    totalPrice: 17.22,
+    recipient: 'JS Masher',
+    orderNumber: '114-2875557-9059409',
+    itemInfo: [{
+      item: 'Gamecube Controller Adabpter. Super Smash Bros Switch Gamecube Adapter for WII U, PC. Support Turbo and Vibration Features. No Driver and No Lag=Gamecube Adapter',
+      itemSeller: null,
+      returnWindow: 'closed Aug 5, 2020',
+      listedPrice: 15.98
+    }
+    ]
+  },
+  {
+    orderDate: 'July 3, 2020',
+    deliveryDate: 'July, 5 2020',
+    multipleItems: true,
+    totalPrice: 138.93,
+    recipient: 'JS Masher',
+    orderNumber: '113-2883177-2648248',
+    itemInfo: [{
+      item: 'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)',
+      itemSeller: null,
+      returnWindow: 'closed Aug 4, 2020',
+      listedPrice: 94.95
+    },
+    {
+      item: 'The Art of Sql',
+      itemSeller: 'Faroult, Stephane',
+      returnWindow: 'closed Aug 4, 2020',
+      listedPrice: 39.99
+    }]
+  }
+];
