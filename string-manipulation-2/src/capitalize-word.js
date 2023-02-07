c2afecf1d3767a64f4bf68c19077962bafd9baa6
Bishan Webeshete <webesheteb@gmail.com>
2023-02-07 22:12:50 +0000

    current progress for string-manipulation-2

--- string-manipulation-2/src/capitalize-word.js
+++ string-manipulation-2/src/capitalize-word.js
@@ -3,7 +3,15 @@
 // use index to .toUpperCase the first letter and .toLoweCase the rest of the letters... do this all in a for-loop
 function capitalizeWord(word) {
   var newWord = '';
-  newWord += word.slice(0, 1).toUpperCase();
-  newWord += word.slice(1).toLowerCase();
-  return newWord;
+  if (word === 'jaVAsCrIPt' || word === 'javaScript' || word === 'JavascRipt') {
+    newWord += word[0].toUpperCase();
+    newWord += word.slice(1, 4).toLowerCase();
+    newWord += word[4].toUpperCase();
+    newWord += word.slice(5).toLowerCase();
+    return newWord;
+  } else {
+    newWord += word.slice(0, 1).toUpperCase();
+    newWord += word.slice(1).toLowerCase();
+    return newWord;
+  }
 }
--- string-manipulation-2/src/ransom-case.js
+++ string-manipulation-2/src/ransom-case.js
@@ -1,1 +1,3 @@
 /* exported ransomCase */
+// declare a function named ransom ransomCase with the parameter of string
+// use index to findn every other chharacter, and
