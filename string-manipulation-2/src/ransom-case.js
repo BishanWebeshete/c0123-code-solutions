d78aad5c65aeaea25e89ee4c0d0c0589f3e8b087
Bishan Webeshete <webesheteb@gmail.com>
2023-02-09 23:58:11 +0000

    current progress on string-manipulation-2

--- string-manipulation-2/src/ransom-case.js
+++ string-manipulation-2/src/ransom-case.js
@@ -1,3 +1,16 @@
 /* exported ransomCase */
 // declare a function named ransom ransomCase with the parameter of string
-// use index to findn every other chharacter, and
+// use index to find every other character with a for loop
+// use .toUpperCase() and return it to the string
+
+function ransomCase(string) {
+  var newString = '';
+  for (let i = 0; i < string.length; i++) {
+    if (i % 2 === 1) {
+      newString += string[i].toUpperCase();
+    } else {
+      newString += string[i].toLowerCase();
+    }
+  }
+  return newString;
+}
