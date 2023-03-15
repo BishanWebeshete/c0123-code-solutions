02eec1b3257b419351c01aee6c3a517dc3718984
Bishan Webeshete <webesheteb@gmail.com>
2023-03-15 16:01:29 +0000

    saved third process.argv to variable and gave user error if they use invalid operation

--- node-module-system/calculate.js
+++ node-module-system/calculate.js
@@ -5,19 +5,16 @@ import divide from './divide.js';
 
 const num = +process.argv[2];
 const num2 = +process.argv[4];
+const action = process.argv[3];
 
-if (process.argv[3] === 'plus') {
+if (action === 'plus') {
   console.log(`result: ${sum(num, num2)}`);
-}
-
-if (process.argv[3] === 'minus') {
+} else if (action === 'minus') {
   console.log(`result: ${subtract(num, num2)}`);
-}
-
-if (process.argv[3] === 'multiply') {
+} else if (action === 'multiply') {
   console.log(`result: ${multiply(num, num2)}`);
-}
-
-if (process.argv[3] === 'divide') {
+} else if (action === 'divide') {
   console.log(`result: ${divide(num, num2)}`);
+} else {
+  console.log('invalid operation');
 }
