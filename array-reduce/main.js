6219a81c0ab288acc1c4bfe0c539f2cb14407693
Bishan Webeshete <webesheteb@gmail.com>
2023-03-16 05:26:34 +0000

    completed array-reduce

---
+++ array-reduce/main.js
@@ -0,0 +1,44 @@
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+const account = [
+  { type: 'deposit', amount: 150 },
+  { type: 'deposit', amount: 20 },
+  { type: 'withdrawal', amount: 5 },
+  { type: 'deposit', amount: 100 },
+  { type: 'withdrawal', amount: 25 },
+  { type: 'withdrawal', amount: 60 }
+];
+
+const traits = [
+  { color: 'yellow' },
+  { type: 'electric' },
+  { name: 'pikachu' },
+  { level: 15 },
+  { trainer: 'ash' }
+];
+
+const sum = numbers.reduce((accumulator, currentValue) => {
+  return accumulator + currentValue;
+}, 0);
+console.log(sum);
+
+const product = numbers.reduce((accumulator, currentValue) => {
+  return accumulator * currentValue;
+});
+console.log(product);
+
+const balance = account.reduce((accumulator, currentValue) => {
+  if (currentValue.type === 'deposit') {
+    return accumulator + currentValue.amount;
+  } else if (currentValue.type === 'withdrawal') {
+    return accumulator - currentValue.amount;
+  } else {
+    return console.error('Transaction is not valid!');
+  }
+}, 0);
+console.log(balance);
+
+const composite = traits.reduce((accumulator, currentValue) => {
+  return Object.assign(accumulator, currentValue);
+});
+console.log(composite);
