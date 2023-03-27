a7c537c0a7ba5645fd76ac37c0f60803044622f2
Bishan Webeshete <webesheteb@gmail.com>
2023-03-27 00:47:10 +0000

    completed es6-classes

--- es6-classes/circle.js
+++ es6-classes/circle.js
@@ -1,14 +1,15 @@
 /* eslint-disable no-undef -- Shape is imported by .html */
 /* exported Circle */
 
-class Circle {
+class Circle extends Shape {
   constructor(radius) {
+    super(Math.PI * Math.pow(radius, 2), 2 * Math.PI * radius);
     this.radius = radius;
   }
 
   print() {
-    console.log(`and here is the radius ${this.radius}`);
+    return `${super.print()} radius ${this.radius}`;
   }
 }
-const sphere = new Circle(155);
-sphere.print();
+const sphere = new Circle(5);
+console.log(sphere.print());
