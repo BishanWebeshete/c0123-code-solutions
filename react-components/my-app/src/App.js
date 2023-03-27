b6622030b91390eaa84e752acc9b40a45a3525e0
Bishan Webeshete <webesheteb@gmail.com>
2023-03-27 23:20:13 +0000

    removed unnecessary attributes

--- react-components/my-app/src/App.js
+++ react-components/my-app/src/App.js
@@ -10,12 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <CustomButton
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        />
+        <CustomButton />
 
       </header>
     </div>
