6735309c9a3ebb0e6c7eeac1b2a287682f44be2c
Bishan Webeshete <webesheteb@gmail.com>
2023-04-11 20:41:28 +0000

    updated big o notation for both function

--- big-o-notation/big-o.js
+++ big-o-notation/big-o.js
@@ -15,7 +15,7 @@ function uniqueLinear(words) {
     }
   }
   return unique;                    // 1 * 1 = O(1)
-} // Big O Notation for uniqueLinear: O(?)
+} // Big O Notation for uniqueLinear: O(n)
 
 function uniqueQuadratic(words) {
   const unique = [];                // 1 * 1 = O(1)
@@ -36,8 +36,8 @@ function uniqueQuadratic(words) {
         isUnique = false;           // 1 * n * n = O(n^2) ~= 0(n^2)
       }
     }
-    if (isUnique) {                 // 1 * n * n = O(n^2) ~= 0(n^2)
-      unique[unique.length] = word; // 3 * n = O(3n) ~= 0(n)
+    if (isUnique) {                 // 1 * n * n = O(n^2) ~= 0(n)
+      unique[unique.length] = word; // 2 * n = O(2n) ~= 0(n)
     }
   }
   return unique;                    // 1 * 1 = O(1)
