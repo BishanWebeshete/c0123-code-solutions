12a543d72379d4b57117cade65e9a791b17c89fb
Bishan Webeshete <webesheteb@gmail.com>
2023-04-14 16:25:32 +0000

    completed main functions

--- data-structures-linked-lists/append.js
+++ data-structures-linked-lists/append.js
@@ -2,4 +2,9 @@
 import LinkedList from './lib/linked-list.js';
 
 export default function append(list, value) {
+  const node = new LinkedList(value);
+  while (list.next !== null) {
+    list = list.next;
+  }
+  list.next = node;
 }
--- data-structures-linked-lists/prepend.js
+++ data-structures-linked-lists/prepend.js
@@ -2,4 +2,7 @@
 import LinkedList from './lib/linked-list';
 
 export default function prepend(list, value) {
+  const newList = new LinkedList(value);
+  newList.next = list;
+  return newList;
 }
