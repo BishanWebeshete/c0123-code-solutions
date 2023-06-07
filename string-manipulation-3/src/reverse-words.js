b054db851e26641bb7dd6e500498b81f78ddf5e4
Bishan Webeshete <webesheteb@gmail.com>
2023-06-07 04:02:21 +0000

    completed reverseWords

--- string-manipulation-3/src/reverse-words.js
+++ string-manipulation-3/src/reverse-words.js
@@ -1,1 +1,15 @@
 /* exported reverseWords */
+
+function reverseWords(string) {
+  var strArr = string.split(' ');
+  var reversed = [];
+  for (var i = 0; i < strArr.length; i++) {
+    var word = strArr[i];
+    var reversing = '';
+    for (var j = word.length - 1; j >= 0; j--) {
+      reversing += word[j];
+    }
+    reversed.push(reversing);
+  }
+  return reversed.join(' ');
+}
